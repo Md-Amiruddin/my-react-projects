792c3c98a9ccbf3dcaeb7857e08d3c8b615af97e
Md Amiruddin <md.amiruddin13@gmail.com>
2024-01-27 20:22:59 +0530

    built redux Todo

---
+++ 11reduxTodo/src/App.jsx
@@ -0,0 +1,13 @@
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+function App() {
+
+  return (
+    <div className='ml-40 mr-40'>
+      <AddTodo/>
+      <Todos/>
+    </div>
+  )
+}
+
+export default App
